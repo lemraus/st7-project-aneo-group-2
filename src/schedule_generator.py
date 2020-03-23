cbd2a66c4d9da8b58a6feb21bb4af4d762785282
natedx <nathan.daix@gmail.com>
2020-03-23 18:18:25 +0100

    In schedule_generator.py function "scheduler" now takes nb_machines and name as arguments and creates first generation chromosomes schedules.

--- src/schedule_generator.py
+++ src/schedule_generator.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 SRC_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(SRC_DIR, "data")
 
+#graphs = ["exampleGraph", "graph", "MediumComplex", "mediumRandom", "smallComplex", "smallRandom"]
+
 def jsonDataFile(name):
     return os.path.join(DATA_DIR, f"{name}.json")
 
-def makeGraph():
+def makeGraph(name):
     G = nx.DiGraph()
-    with open(jsonDataFile("smallRandom")) as jsonFile:
+    with open(jsonDataFile(name)) as jsonFile:
         dataDict = json.load(jsonFile)
         for key, value in dataDict["nodes"].items():
             G.add_node(key, data=value["Data"])
@@ -20,21 +22,23 @@ def makeGraph():
 
     return G
 
-def scheduler():
-    # We start with a bfs search
-    G = makeGraph()
+def scheduler(nb_machines, name):
+    G = makeGraph(name)
     bfs = nx.bfs_edges(G, "1")
-    schedule = [("1", "1")]
-    buff =  0
+    schedule = [("1", "0")]
+    i = 1
     for (parent, child) in bfs:
-        schedule.append(("1", child))
+        schedule.append((child, str(i % nb_machines)))
+        i += 1
 
     return schedule
+
 # The bfs algorithm is bfs_edges(G, source)
 
 # nx.draw_kamada_kawai(G, with_labels = True)
-plt.draw()
-plt.show()
+# plt.draw()
+# plt.show()
 
+# Pour tester:
 if __name__ == "__main__":
-    print(scheduler())
+    print(scheduler(5, "mediumRandom"))
