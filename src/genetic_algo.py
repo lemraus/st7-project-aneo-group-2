cbf6be1d7abfbfa75ccc6b9994e5e5ab0377aff1
natedx <nathan.daix@gmail.com>
2020-04-03 02:19:44 +0200

    The genetic algorithm works! Need to work on modifying values and penalties in the cost function though, as the entire population seems to converge towards the 1 machine solution as of now.
    Modified mutate.py so that it now works with the toolbox system.

--- src/genetic_algo.py
+++ src/genetic_algo.py
@@ -1,5 +1,8 @@
-from deap import creator, base
+import random
 
+from deap import creator, base, tools
+
+from src.cost_func import cost_func as evaluate
 from src.crossover import crossover_in_place as mate
 from src.mutate import mutate_in_place as mutate
 from src.schedule_generator import init_generation, makeGraph
@@ -14,9 +17,12 @@ creator.create("Individual", list, fitness=creator.FitnessMin)
 NB_POP, MAX_MACH = 10, 10
 MACHINES_MUTATION_PROBABILITY = 0.2
 MUTATION_PROBABILITY = 0.1
+CXPB = 0.5
+MUTPB = 0.5
+NGEN = 10
 
 # Let us build the graph only once in order to save time
-graph_name = "smallRandom"
+graph_name = "mediumRandom"
 task_graph = makeGraph(graph_name)
 
 
@@ -43,13 +49,36 @@ toolbox.register("individual_guess", initChromosome, creator.Individual)
 toolbox.register("population_guess", initPopulation, list, toolbox.individual_guess, graph_name)
 toolbox.register("mutate", mutate, MUTATION_PROBABILITY, MACHINES_MUTATION_PROBABILITY)
 toolbox.register("mate", mate)
-population = toolbox.population_guess()
+toolbox.register("evaluate", evaluate, task_graph)
+toolbox.register("select", tools.selTournament, tournsize=3)
+pop = toolbox.population_guess()
 
+# To the heart of the genetic algorithm
 
-# Test
-ind1, ind2 = population[0], population[9]
+for g in range(NGEN):
+    # Select the next generation individuals
+    offspring = toolbox.select(pop, len(pop))
+    # Clone the selected individuals
+    offspring = [toolbox.clone(ind) for ind in offspring]
 
-for i in range(20):
-    child1, child2 = [toolbox.clone(ind) for ind in (ind1, ind2)]
-    toolbox.mate(child1, child2)
-    print(f"{i} child1", child1,"\n", " child2", child2)
+    # Apply crossover on the offspring
+    for child1, child2 in zip(offspring[::2], offspring[1::2]):
+        if random.random() < CXPB:
+            toolbox.mate(child1, child2)
+            del child1.fitness.values
+            del child2.fitness.values
+
+    # Apply mutation on the offspring
+    for mutant in offspring:
+        if random.random() < MUTPB:
+            toolbox.mutate(mutant)
+            del mutant.fitness.values
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    # The population is entirely replaced by the offspring
+    pop[:] = offspring
