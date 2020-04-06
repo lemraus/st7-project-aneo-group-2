4d15f3af87da3c87d8d60d973fd5462808e35d61
Mathis Pierron <mathis.pierron@student-cs.fr>
2020-04-06 11:21:43 +0200

    the number of machines always changes when a mutation occurs

--- src/crossover.py
+++ src/crossover.py
@@ -25,17 +25,16 @@ def crossover(chromosome1, chromosome2):
 def crossover_in_place(chromosome1, chromosome2):
     n = len(chromosome1)
     crossover_point = randint(0, n)
-    new_chromosome = chromosome1[:crossover_point]
 
     counter1 = crossover_point
     counter2 = crossover_point
     for i in range(n):
         gene1 = chromosome2[i]
         gene2 = chromosome1[i]
-        if not contains_task(chromosome1[:counter1], gene1[0]):
+        if not contains_task(chromosome1[:crossover_point], gene1[0]):
             chromosome1[counter1] = gene1
             counter1 += 1
-        if not contains_task(chromosome2[:counter2], gene2[0]):
+        if not contains_task(chromosome2[:crossover_point], gene2[0]):
             chromosome2[counter2] = gene2
             counter2 += 1
     return chromosome1, chromosome2
