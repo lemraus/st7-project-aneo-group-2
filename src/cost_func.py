6d829afa6c46917038c8185881f6c0f1fcf6efba
Mathis <mathis.pierron@student-cs.fr>
2020-04-07 17:22:19 +0200

    commenting the cost function

--- src/cost_func.py
+++ src/cost_func.py
@@ -6,7 +6,9 @@ def cost_func(graph, array, max_duration):
     BOOTING_TIME = 5*60
     PENALTY_MULTIPLIER = 2
     
+    # Number of machines used by the individual
     machines_number = max(map(lambda x: x[1], array))
+
     # Array of finishing times of the last task of a given machine
     time_machine = [0] * (machines_number + 1)
 
@@ -23,18 +25,19 @@ def cost_func(graph, array, max_duration):
         finishing_time = starting_time + duration
         time_machine[machine] = finishing_time
         time_task[int(task)] = finishing_time
-        if last_task_ending_time == 0:  # First task of the machine
+        if last_task_ending_time == 0:  # If it is the first task of the machine
             total_cost += COST * (BOOTING_TIME + duration)
-            if starting_time < BOOTING_TIME:  # We have to boot the machine
+            if starting_time < BOOTING_TIME:  # We have to boot the machine before executing the task
                 time_machine[machine] += BOOTING_TIME
                 time_task[int(task)] += BOOTING_TIME
-        elif starting_time - last_task_ending_time > BOOTING_TIME:  # We have to reboot the machine
+        elif starting_time - last_task_ending_time > BOOTING_TIME:  # We have to reboot the machine before executing the task
             total_cost += COST * (BOOTING_TIME + duration)
-        else:  # The machine stays on
+        else:  # The machine stays on between the last task and this task
             total_cost += COST * (finishing_time - last_task_ending_time)
-        # print(f'Tâche {task}: coût de {total_cost}.')
 
     total_duration = max(time_machine)
+
+    # Penalty when the individual does not respect the constraint
     if total_duration > max_duration:
         total_cost *= PENALTY_MULTIPLIER
     return (total_cost, total_duration)
